07ab5073e2d1e510096a8da047e8bb9e3e2e287a
Maxwell G <gotmax@e.email>
2022-11-22 22:11:50 -0600

    Add meeting ping list and script

---
+++ meeting/meeting-people.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import sys
+from pathlib import Path
+
+HAS_PYPERCLIP = False
+try:
+    import pyperclip
+except ImportError:
+    pass
+else:
+    HAS_PYPERCLIP = True
+
+
+def main():
+    path = Path(__file__).parent.joinpath("meeting-people.txt")
+    with path.open(encoding="utf-8") as fp:
+        people = sorted(
+            [
+                person
+                for line in fp
+                if (person := line.strip()) and not line.startswith("#")
+            ]
+        )
+    message = "{}: The Ansible community meeting is starting now!".format(
+        ", ".join(people)
+    )
+    message += (
+        "\nThe ping list is stored at https://kutt.it/meeting-people."
+        " Feel free to add or remove yourself."
+    )
+    print(message)
+    if HAS_PYPERCLIP and "-c" in sys.argv[1:]:
+        pyperclip.copy(message)
+
+
+if __name__ == "__main__":
+    main()
